b234d4a0478c764ba4edc074bdaa09e9b4be17e3
Todd Turnbull <shingaling@gmail.com>
2016-07-04 15:06:11 -0400

    Python tools, Git, and Git config

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# software
+
+sudo apt update
+sudo apt install -y build-essential git python-pip python-setuptools python-virtualenv
+
+# git
+
+git config --global user.name "Todd Turnbull"
+git config --global user.email "shingaling@gmail.com"
+
