49253e67797c4a72c9254776dfabc067ad3beaa2
Todd Turnbull <shingaling@gmail.com>
2016-08-24 15:50:44 -0400

    Fonts and graphics

---
+++ setup-graphics.sh
@@ -0,0 +1,13 @@
+# software
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y --no-upgrade gimp
+sudo apt install -y --no-upgrade inkscape
+sudo apt install -y --no-upgrade darktable
+sudo apt install -y --no-upgrade scribus
+sudo apt install -y --no-upgrade typecatcher
+sudo apt install -y --no-upgrade ttf-mscorefonts-installer
+sudo apt install -y --no-upgrade fonts-liberation
+sudo apt install -y --no-upgrade fonts-noto
+
